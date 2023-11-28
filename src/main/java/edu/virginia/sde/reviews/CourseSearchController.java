bb892fe14cc1e17604d4ddc1719282fb5c7752f8
cbrockenbrough21 <cqt5de@virginia.edu>
2023-11-28 18:07:44 -0500

    working on course search page

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -1,18 +1,33 @@
 package edu.virginia.sde.reviews;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourseSearchController {
     @FXML
     private TableView<Course> tableView;
 
+    public void initialize(){
+        updateTable();
+    }
     public void handleSearchButton(){
         //logic for search for anything in all three of the fields
+        //updateTable();
     }
 
     private void updateTable(){
+        List<Course> courseList = new ArrayList<>();
+        Course myCourse = new Course("CS", 2100, "DSA1");
+        courseList.add(myCourse);
+        ObservableList<Course> obsList = FXCollections.observableList(courseList);
         tableView.getItems().clear();
         tableView.getItems().addAll(obsList);
     }
