904ed7cb1a5391160d4145bf1c1ca630872f4a79
cbrockenbrough21 <cqt5de@virginia.edu>
2023-11-28 17:32:51 -0500

    working on course search page

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -5,8 +5,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 public class CourseSearchController {
+    @FXML
+    private TableView<Course> tableView;
 
     public void handleSearchButton(){
         //logic for search for anything in all three of the fields
+    }
+
+    private void updateTable(){
+        tableView.getItems().clear();
+        tableView.getItems().addAll(obsList);
     }
 }
