32f2cace380c2f8365ca567a067e89c2340a4aed
owarren22 <rgx6mx@virginia.edu>
2023-11-27 21:04:26 -0500

    some comments

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -6,6 +6,8 @@ import javafx.stage.Stage;
 
 public class LoginController {
 
+    private User activeUser;
+
     public void displayLogin(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -13,4 +15,7 @@ public class LoginController {
         stage.setScene(scene);
         stage.show();
     }
+
+    //create a function for handling the login button being pressed
+        //see if that pair exists in the database, if not prompt an error
 }
