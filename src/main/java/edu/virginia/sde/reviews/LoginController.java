b6995ef61f338e13ae385e715cc18a88544ac20c
zcx6na <zcx6na@virginia.edu>
2023-11-27 22:34:18 -0500

    first try of adding feature to accommodate for incorrect username or password

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,11 +1,15 @@
 package edu.virginia.sde.reviews;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class LoginController {
 
+    @FXML
+    public Label loginError;
     private User activeUser;
 
     public void displayLogin(Stage stage) throws Exception {
@@ -14,6 +18,19 @@ public class LoginController {
         //stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void handleLoginButton() {
+        //Some type of select queries and logic to see
+        //if the user exists and log them in if so
+
+        // if user does not exist
+        handleLoginError();
+    }
+
+    public void handleLoginError() {
+        loginError.setStyle("-fx-text-fill: red;");
+        loginError.setText("Incorrect Username or Password. Please try again.");
     }
 
     //create a function for handling the login button being pressed
