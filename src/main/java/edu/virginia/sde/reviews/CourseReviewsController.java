f1423c08584054496a81dfd12777532f2573c046
zcx6na <zcx6na@virginia.edu>
2023-11-29 18:15:39 -0500

    basic functions to course review controller to get rid of errors

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
@@ -1,4 +1,15 @@
 package edu.virginia.sde.reviews;
 
+import javafx.stage.Stage;
+
 public class CourseReviewsController {
+
+    private User activeUser;
+
+    private Stage primaryStage;
+    public void setActiveUser(User activeUser) { this.activeUser = activeUser; }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
 }
