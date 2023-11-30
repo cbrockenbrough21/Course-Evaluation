5cf307f135a0ac793ef59dc4c7ca48ac22bbd583
owarren22 <rgx6mx@virginia.edu>
2023-11-30 14:02:57 -0500

    made service file for course reviews controller

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsService.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsService.java
@@ -1,5 +1,31 @@
 package edu.virginia.sde.reviews;
 
+import java.sql.SQLException;
+import java.util.*;
+
 public class CourseReviewsService {
+
+    public void initialize(){
+        DatabaseConnection databaseConnection = null;
+        try {
+            databaseConnection = new DatabaseConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Review> getReviewList(Course course){
+        int courseId = course.getCourseId();
+        List<Review> reviewList = null;
+
+        DatabaseConnection databaseConnection = null;
+        try {
+            databaseConnection = new DatabaseConnection();
+            reviewList = databaseConnection.getReviews(courseId);
+            return reviewList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
