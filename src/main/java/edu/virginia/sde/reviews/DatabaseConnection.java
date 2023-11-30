13defc5aa7038b2201dee457c77681404a65e619
zcx6na <zcx6na@virginia.edu>
2023-11-30 11:20:49 -0500

    took out unnecessary debugging print statement

--- src/main/java/edu/virginia/sde/reviews/DatabaseConnection.java
+++ src/main/java/edu/virginia/sde/reviews/DatabaseConnection.java
@@ -212,7 +212,6 @@ public class DatabaseConnection {
     }
 
     public void disconnect() throws SQLException {
-        System.out.println("connection closes");
         connection.close();
     }
 }
