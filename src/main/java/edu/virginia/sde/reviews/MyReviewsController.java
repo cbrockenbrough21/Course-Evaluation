b795b169aa9ffeb1706a642d60b4b99b173ace99
Riley <rileymariewilson@gmail.com>
2023-12-04 23:13:10 -0500

    deleted comments/print statements

--- src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsController.java
@@ -56,7 +56,6 @@ public class MyReviewsController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //System.out.println("Clicked on row with title: " + selectedReview.getSubject());
     }
     public void handleBackButton() {
         //scene switch back to course search from My Reviews
